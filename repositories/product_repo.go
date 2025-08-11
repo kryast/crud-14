00d37239835c894bd8083d26854765a2ee153263
Ahmad Syarifuddin <oden.psht@gmail.com>
2025-08-11 14:32:42 +0700

    Create Product Repositories

---
+++ repositories/product_repo.go
@@ -0,0 +1,13 @@
+package repositories
+
+import "gorm.io/gorm"
+
+type ProductRepository interface{}
+
+type productRepository struct {
+	db *gorm.DB
+}
+
+func NewProductRepository(db *gorm.DB) ProductRepository {
+	return &productRepository{db}
+}
